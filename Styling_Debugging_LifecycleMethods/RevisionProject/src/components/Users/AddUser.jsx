5d1f44ec7e1ac011a460a08c267906980d4966d1
Shivamahajan045 <111225580+Shivamahajan045@users.noreply.github.com>
2026-01-01 14:40:02 +0530

    Reusable Card

--- Styling_Debugging_LifecycleMethods/RevisionProject/src/components/Users/AddUser.jsx
+++ Styling_Debugging_LifecycleMethods/RevisionProject/src/components/Users/AddUser.jsx
@@ -1,17 +1,22 @@
+import React from "react";
+import Card from "../UI/Card";
+import "./AddUser.css";
+
 const AddUser = () => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
+
   return (
-    <>
+    <Card className="input">
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
+        <input id="username" type="text" />
         <label htmlFor="age">Age</label>
-        <input type="number" id="age" />
+        <input id="age" type="number" />
         <button type="submit">Add User</button>
       </form>
-    </>
+    </Card>
   );
 };
 
