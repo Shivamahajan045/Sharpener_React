c9db0874376bfd1c7e68afa9a238191033e618ac
Shivamahajan045 <111225580+Shivamahajan045@users.noreply.github.com>
2026-01-01 16:30:45 +0530

    Collecting User Inputs

--- Styling_Debugging_LifecycleMethods/RevisionProject/src/components/Users/AddUser.jsx
+++ Styling_Debugging_LifecycleMethods/RevisionProject/src/components/Users/AddUser.jsx
@@ -2,19 +2,41 @@ import React from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import "./AddUser.css";
+import { useState } from "react";
 
 const AddUser = () => {
+  const [userData, setUserData] = useState({ userName: "", age: "" });
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(userData.userName, userData.age);
+
+    setUserData({ userName: "", age: "" });
   };
 
   return (
     <Card className="input">
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          name="userName"
+          type="text"
+          value={userData.userName}
+          onChange={(e) =>
+            setUserData({ ...userData, [e.target.name]: e.target.value })
+          }
+        />
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          name="age"
+          onChange={(e) =>
+            setUserData({ ...userData, [e.target.name]: e.target.value })
+          }
+          value={userData.age}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
