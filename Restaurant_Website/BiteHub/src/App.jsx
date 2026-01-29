398e3363b87b5d10482a1c115114d4985f2291ce
Shivamahajan045 <111225580+Shivamahajan045@users.noreply.github.com>
2026-01-29 15:36:16 +0530

    Making the Items visible

--- Restaurant_Website/BiteHub/src/App.jsx
+++ Restaurant_Website/BiteHub/src/App.jsx
@@ -1,11 +1,11 @@
 import Header from "./components/Layout/Header";
-import MealsSummary from "./components/Meals/MealsSummary";
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <>
       <Header />
       <main>
-        <MealsSummary />
+        <Meals />
       </main>
     </>
   );
