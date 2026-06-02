7d0e5f5da774e87b2188da18d2a4966fe71d608f
Shivamahajan045 <111225580+Shivamahajan045@users.noreply.github.com>
2026-06-02 20:13:23 +0530

    added error state

--- fetchMovies/01-starting-project/src/App.js
+++ fetchMovies/01-starting-project/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -6,9 +6,14 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [retrying, setRetrying] = useState(false);
+  const [cancelRetry, setCancelRetry] = useState(false);
+
   const fetchMoviesHandler = async () => {
     try {
       setIsLoading(true);
+      setError("");
       const res = await fetch("https://swapi.py4e.com/api/films/");
       const data = await res.json();
 
@@ -21,13 +26,26 @@ function App() {
         };
       });
       setMovies(transformedMovies);
+      setRetrying(false);
+      setError("");
     } catch (err) {
-      console.log(err);
-      throw err;
+      setError("Something went wrong....Retrying");
+      setRetrying(true);
     } finally {
       setIsLoading(false);
     }
   };
+
+  useEffect(() => {
+    if (!retrying || cancelRetry) return;
+    const id = setInterval(() => {
+      fetchMoviesHandler();
+    }, 5000);
+
+    return () => {
+      clearInterval(id);
+    };
+  }, [retrying, cancelRetry]);
 
   return (
     <React.Fragment>
@@ -39,6 +57,20 @@ function App() {
           <div className="loader"></div>
         ) : (
           <MoviesList movies={movies} />
+        )}
+        {error && <h3>{error}</h3>}
+      </section>
+      <section>
+        {retrying && (
+          <button
+            onClick={() => {
+              setCancelRetry(true);
+              setRetrying(false);
+              setError("Retry cancelled");
+            }}
+          >
+            Cancel Retry
+          </button>
         )}
       </section>
     </React.Fragment>
