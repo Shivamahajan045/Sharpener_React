4aadf8e22880d515ad6894347c44abd41d4cf420
Shivamahajan045 <111225580+Shivamahajan045@users.noreply.github.com>
2026-06-02 14:11:03 +0530

    implemented loader

--- fetchMovies/01-starting-project/src/App.js
+++ fetchMovies/01-starting-project/src/App.js
@@ -5,19 +5,28 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const fetchMoviesHandler = async () => {
-    const res = await fetch("https://swapi.py4e.com/api/films/");
-    const data = await res.json();
+    try {
+      setIsLoading(true);
+      const res = await fetch("https://swapi.py4e.com/api/films/");
+      const data = await res.json();
 
-    let transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+      let transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (err) {
+      console.log(err);
+      throw err;
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -25,8 +34,12 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        <MoviesList movies={movies} />
+      <section style={{ display: "flex", justifyContent: "center" }}>
+        {isLoading ? (
+          <div className="loader"></div>
+        ) : (
+          <MoviesList movies={movies} />
+        )}
       </section>
     </React.Fragment>
   );
